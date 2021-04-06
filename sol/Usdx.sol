35a08a746a1ad269133d3d8b2114758b06a1ecfd
royalfork <joe@royalfork.org>
2021-04-06 09:40:00 -0500

    Update Usdx.sol documentation

--- sol/Usdx.sol
+++ sol/Usdx.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.2;
 import "./chainlink/evm-contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
 import "./openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "./openzeppelin-contracts/contracts/access/Ownable.sol";
-/* TODO: Do these need to be imported?  Can only the needed functions be included? */
 import "./openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
 import "./openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
 
@@ -18,23 +17,20 @@ import "./openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
  *    \______/  \______/ \_______/ \__|  \__|
  *
  *   USDX is a USD based stablecoin with the following properties:
- *   - Eth sent to the USDX contract is minted into USDX at the current
- *     Eth/USD exchange rate.
+ *   - Eth sent to the usdx contract is locked, and USDX is minted
+ *     into the sender's account at the current eth/usd exchange rate.
+ *   - Locked eth can be redeemed by the original sender by burning USDX
+ *     at the originally minted price.
  *
- *   TODO: ADD MORE THINGS HERE!
- *
- *   Note: Owner is able to set the Eth/USD oracle.
+ *   Note: Owner is able to set the eth/usd oracle.
  */
 contract USDX is ERC20, Ownable {
 	using SafeMath for uint256;
 	using SafeCast for int256;
 	uint8 constant FEED_DECS = 8;
-	/* TODO: s/"ETH/USD"/"USD/ETH"? */
-	// ETH/USD exchange rate feed.
-	AggregatorV3Interface public ethUsdPriceFeed;
+	AggregatorV3Interface public usdPriceFeed;
 	uint8 public feedDecs;
 
-	/* TODO: Consider better var names.  Deposited? */
 	struct account {
 		uint256 locked;
 		uint256 mint;
@@ -45,18 +41,19 @@ contract USDX is ERC20, Ownable {
 		setFeed(_priceFeed);
 	}
 
-	// _newFeed's decimals parameter must be immutable.
+	// _newFeed's decimals parameter must immutably be set to 8.
 	function setFeed(address _newFeed) public onlyOwner {
 		AggregatorV3Interface newFeed = AggregatorV3Interface(_newFeed);
 		require(newFeed.decimals() == FEED_DECS);
-		ethUsdPriceFeed = newFeed;
+		usdPriceFeed = newFeed;
 	}
 
-	// Received eth is minted into usdx at the current ETH/USD
-	// exchange rate. Note: msg.sender must be payable to allow
-	// redemption of deposited eth.
+	// Received eth is minted into usdx at the current eth/usd
+	// exchange rate.
+	// Note: msg.sender must be payable to allow redemption of
+	// deposited eth.
 	receive() external payable {
-		(,int256 rate,,,) = ethUsdPriceFeed.latestRoundData();
+		(,int256 rate,,,) = usdPriceFeed.latestRoundData();
 		uint256 toMint = weiToUSDX(msg.value, rate);
 		account storage acct = accounts[msg.sender];
 		acct.locked += msg.value;
@@ -64,18 +61,18 @@ contract USDX is ERC20, Ownable {
 		_mint(msg.sender, toMint);
 	}
 
-	// Redeems usdx back into eth.  This method should only be called
-	// by sender's who have previously directly sent eth into this
-	// contract to mint usdx.  The amount of eth redeemed is capped to
-	// the amount of eth msg.sender has previously sent.  The amount
-	// of eth redeemed is independent of the current ETH/USD price;
-	// it's based purely on the ratio of previously sent eth and
+	// Redeems usdx back into eth.  This method can only be called by
+	// senders who have previously directly sent eth into this
+	// contract.  The amount of eth redeemed is capped to the amount
+	// of eth the sender has previously sent.  The amount of eth
+	// redeemed is unrelated to the current ETH/USD price; it's based
+	// purely on the ratio of previously sent eth and previously
 	// minted usdx (ie: if 1 eth was previously received by this
 	// contract to mint 1000usdx, 500 usdx is able to be redeemed for
-	// .5 eth).  If _amount is 0, msg.sender's full usdx balance will
-	// be used to redeem eth.  Otherwise, a defined _amount of usdx is
-	// used to redeem into eth.
-	/* TODO: Consider "unlock"? function name? */
+	// .5 eth regardless of whether usd/eth price has decreased to
+	// 800usd/eth).  If _amount is 0, msg.sender's full usdx balance
+	// will be used to redeem eth.  Otherwise, _amount is the maximum
+	// amount of usdx used to redeem into eth.
 	function redeem(uint256 _amount) public returns (uint256) {
 		account storage acct = accounts[msg.sender];
 		require(acct.locked > 0, "nothing to redeem");
@@ -101,11 +98,10 @@ contract USDX is ERC20, Ownable {
 	}
 
 	// Appreciation occurs when previously locked eth appreciates in
-	// usd price. When this occurs, the amount of appreciation can be
-	// collected as USDX, up to _limit.  A _limit of 0 collects all
-	// available appreciation. To redeem the locked eth, both the
-	// principle usdx mint and any collected appreciation must be
-	// returned.
+	// usd price. The amount of appreciation can be collected as new
+	// usdx, up to _limit.  A _limit of 0 collects all available
+	// appreciation. To redeem the locked eth, both the principle usdx
+	// mint and any collected appreciation must be returned.
 	function collectAppreciation(uint256 _limit) public returns (uint256) {
 		account storage acct = accounts[msg.sender];
 		(bool ok, uint256 appr) = acctAppreciation(acct);
@@ -137,12 +133,12 @@ contract USDX is ERC20, Ownable {
 	}
 
 	function acctAppreciation(account storage _acct) private view returns (bool, uint256) {
-		(,int256 rate,,,) = ethUsdPriceFeed.latestRoundData();
+		(,int256 rate,,,) = usdPriceFeed.latestRoundData();
 		uint256 lockedVal = weiToUSDX(_acct.locked, rate);
 		return SafeMath.trySub(lockedVal, _acct.mint);
 	}
 
-	function weiToUSDX(uint256 _wei, int256 _rate) private view returns (uint256) {
+	function weiToUSDX(uint256 _wei, int256 _rate) private pure returns (uint256) {
 		return _wei.mul(_rate.toUint256()).div(10**FEED_DECS);
 	}
 
